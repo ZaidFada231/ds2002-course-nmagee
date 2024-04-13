258c92d15354ddaf9ba190ced142177460e43053
ZaidFada231 <74735647+ZaidFada231@users.noreply.github.com>
2024-04-12 20:20:00 -0400

    Update mongodb.py

--- mywork/lab9/mongodb.py
+++ mywork/lab9/mongodb.py
@@ -1,23 +1,31 @@
+import os
 from pymongo import MongoClient
 
-#your mongo clinet connection string
-client = MongoClient(' ')
+def main():
+    MONGOPASS = os.getenv("MONGOPASS")
+    uri = "mongodb+srv://cluster0.pnxzwgz.mongodb.net/"
+    client = MongoClient(
+        uri,
+        username="nmagee",
+        password=MONGOPASS,
+        connectTimeoutMS=200,
+        retryWrites=True,
+    )
 
-db = client['ycq2zz']
+    db = client["ycq2zz"]
+    collection = db["soccer_players_class"]
 
-collection = db['soccer_players_class']
+    players = [
+        {"name": "Cristiano Ronaldo", "position": "Forward", "teams": ["Sporting CP", "Manchester United", "Real Madrid", "Juventus"]},
+        {"name": "Lionel Messi", "position": "Forward", "teams": ["FC Barcelona", "Paris Saint-Germain"]},
+        {"name": "Neymar", "position": "Forward", "teams": ["Santos", "FC Barcelona", "Paris Saint-Germain"]},
+        {"name": "Virgil van Dijk", "position": "Defender", "teams": ["Groningen", "Celtic", "Southampton", "Liverpool"]},
+        {"name": "Kevin De Bruyne", "position": "Midfielder", "teams": ["Genk", "Chelsea", "Werder Bremen", "VfL Wolfsburg", "Manchester City"]}
+    ]
 
-players = [
-    {"name": "Cristiano Ronaldo", "position": "Forward", "teams": ["Sporting CP", "Manchester United", "Real Madrid", "Juventus"]},
-    {"name": "Lionel Messi", "position": "Forward", "teams": ["FC Barcelona", "Paris Saint-Germain"]},
-    {"name": "Neymar", "position": "Forward", "teams": ["Santos", "FC Barcelona", "Paris Saint-Germain"]},
-    {"name": "Virgil van Dijk", "position": "Defender", "teams": ["Groningen", "Celtic", "Southampton", "Liverpool"]},
-    {"name": "Kevin De Bruyne", "position": "Midfielder", "teams": ["Genk", "Chelsea", "Werder Bremen", "VfL Wolfsburg", "Manchester City"]}
-]
+    collection.insert_many(players)
 
-collection.insert_many(players)
+    result = collection.find().limit(3)
 
-result = collection.find().limit(3)
-
-for doc in result:
-    print(doc)
+    for doc in result:
+        print(doc)
